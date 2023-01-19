84818e0c6c32e4a5868f9281c813679a83a82072
Diana Lin <linjdiana@gmail.com>
2023-01-18 20:11:39 -0800

    complete

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+
+function superbowlWin(record) {
+
+ const result = record.find((winning) => {
+        if(winning.result === "W") {
+        return true
+ } else {return false}
+})
+
+if(result) { return result.year} else { return undefined}
+
+}
